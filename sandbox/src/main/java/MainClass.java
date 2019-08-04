ce1556b026b93608e22f62c4cb5bd676bc8ae75d
Anna Yargina <terrajanis@yandex.ru>
2019-08-04 20:18:31 +0300

    First task (Point)

--- sandbox/src/main/java/MainClass.java
+++ sandbox/src/main/java/MainClass.java
@@ -4,17 +4,11 @@ public class MainClass {
 
         Point p1 = new Point (3, 2);
         Point p2 = new Point (7, 8);
-        double x = (p1.x - p2.x) * (p1.x - p2.x);
-        double y = (p1.y - p2.y) * (p1.y - p2.y);
 
-     System.out.println(distance(x, y));
+     System.out.println(p1.distance(p2));
     }
 
 
-    public static double distance(double x, double y) {
-        double sqrt = (double) Math.sqrt(x + y);
-        return sqrt;
-    }
 
 }
 
--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -7,4 +7,9 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+
+    public double distance(Point p) {
+        double sqrt = (double) Math.sqrt(((this.x - p.x) * (this.x - p.x)) + ((this.y - p.y) * (this.y - p.y)));
+        return sqrt;
+    }
 }
