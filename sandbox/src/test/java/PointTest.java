f0e15ffa766a4635f95c7c644327771a21de0fed
Anna Yargina <terrajanis@yandex.ru>
2019-08-05 22:08:55 +0300

    Test (task3)

--- sandbox/src/test/java/PointTest.java
+++ sandbox/src/test/java/PointTest.java
@@ -9,4 +9,18 @@ public class PointTest {
         Point p2 = new Point (7, 8);
         Assert.assertEquals(p1.distance(p2), 7.211102550927978);
     }
+
+    @Test
+    public void testArea2() {
+        Point p1 = new Point (3, 2);
+        Point p2 = new Point (7, 8);
+        Assert.assertEquals(p1.distance(p2), 5);
+    }
+    
+    @Test
+    public void testArea3() {
+        Point p1 = new Point (1, 1);
+        Point p2 = new Point (2, 2);
+        Assert.assertEquals(p1.distance(p2), 1.4142135623730951);
+    }
 }
