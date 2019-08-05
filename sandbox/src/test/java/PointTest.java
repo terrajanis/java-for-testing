11b3b4c2072601bd82353970d74c7430490d87f3
Anna Yargina <terrajanis@yandex.ru>
2019-08-05 22:02:32 +0300

    Test1

--- sandbox/src/test/java/PointTest.java
+++ sandbox/src/test/java/PointTest.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 public class PointTest {
 
     @Test
-    public void testArea() {
+    public static void testArea() {
         Point p1 = new Point (3, 2);
         Point p2 = new Point (7, 8);
         Assert.assertEquals(p1.distance(p2), 7.211102550927978);
