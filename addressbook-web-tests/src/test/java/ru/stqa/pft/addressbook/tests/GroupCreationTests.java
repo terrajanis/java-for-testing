5c5380a4ccbffc21216cad98fd914db57a95342f
Anna Yargina <terrajanis@yandex.ru>
2019-08-24 15:10:56 +0300

    Сортировка групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.models.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -21,6 +22,9 @@ public class GroupCreationTests extends TestBase {
 
     group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
+    Comparator<? super GroupData> byID = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byID);
+    after.sort(byID);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
