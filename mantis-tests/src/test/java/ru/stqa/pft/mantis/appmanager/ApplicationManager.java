023071e41a62483a30da2540dd9ef2f35e00a2e2
Anna Yargina <terrajanis@yandex.ru>
2019-09-28 21:14:14 +0300

    Homework 19

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -25,6 +25,7 @@ public class ApplicationManager {
     private JamesHelper jamesHelper;
     private ChangeHelper changeHelper;
     private DbHelper dbHelper;
+    private SoapHelper soapHelper;
 
 
     public ApplicationManager(String browser) {
@@ -104,6 +105,13 @@ public class ApplicationManager {
             dbHelper = new DbHelper(this);
         }
         return dbHelper;
+    }
+
+    public SoapHelper soap() {
+        if (soapHelper == null) {
+            soapHelper = new SoapHelper(this);
+        }
+        return soapHelper;
     }
     
     public WebDriver getDriver() {
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -1,11 +1,18 @@
 package ru.stqa.pft.mantis.tests;
 
+import biz.futureware.mantis.rpc.soap.client.IssueData;
+import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import javax.xml.rpc.ServiceException;
 import java.io.File;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
 
 public class TestBase {
 
@@ -23,5 +30,23 @@ public class TestBase {
         app.stop();
     }
 
+    public void skipIfNotFixed(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+        if (isIssueOpen(issueId)) {
+            throw new SkipException("Ignored because of issue " + issueId);
+        }
+    }
 
+    public boolean isIssueOpen(int issueId) throws RemoteException, MalformedURLException, ServiceException {
+        MantisConnectPortType ms = app.soap().getMantisConnect();
+        IssueData issue = ms.mc_issue_get(app.getProperty("web.adminLogin"), app.getProperty("web.adminPassword"), BigInteger.valueOf(issueId));
+        String issueStatus = issue.getStatus().getName();
+        if (issueStatus.equals("open")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
+
+
+
