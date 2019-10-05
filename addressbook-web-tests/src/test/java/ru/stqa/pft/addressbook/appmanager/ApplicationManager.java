1e52e1606ba41b2ddbc28581746ea5a3f7266926
Anna Yargina <terrajanis@yandex.ru>
2019-10-05 15:47:18 +0300

    Homework 21 ver 3

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -48,7 +48,7 @@ public class ApplicationManager {
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setBrowserName(browser);
             capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win7")));
-            wd = new RemoteWebDriver(new URL(properties.getProperty("selemium.server")), capabilities);
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
 
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
