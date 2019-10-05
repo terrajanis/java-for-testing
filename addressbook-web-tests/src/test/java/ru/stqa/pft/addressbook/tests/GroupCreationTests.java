a45850cd1b3703300b2b87ad69a6253093741678
Anna Yargina <terrajanis@yandex.ru>
2019-10-05 17:01:09 +0300

    small fixes

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -24,7 +24,7 @@ public class GroupCreationTests extends TestBase {
 
     @DataProvider
     public Iterator<Object[]> validGroupsFromXml() throws IOException {
-        try(BufferedReader reader = new BufferedReader (new FileReader(new File("src/rest/resources/groups.xml")))) {
+        try(BufferedReader reader = new BufferedReader (new FileReader(new File("src/test/resources/groups.xml")))) {
             String xml = "";
             String line = reader.readLine();
             while (line != null) {
@@ -40,7 +40,7 @@ public class GroupCreationTests extends TestBase {
 
     @DataProvider
     public Iterator<Object[]> validGroupsFromJson() throws IOException {
-        try(BufferedReader reader = new BufferedReader (new FileReader(new File("src/rest/resources/groups.json")))) {
+        try(BufferedReader reader = new BufferedReader (new FileReader(new File("src/test/resources/groups.json")))) {
             String json = "";
             String line = reader.readLine();
             while (line != null) {
